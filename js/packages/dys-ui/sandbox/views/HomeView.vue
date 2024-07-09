379ac38f305883b87b68e34ad87eb7b4f9522ab9
David Soards <david@soards.me>
2024-07-09 02:50:45 -0400

    ui: organize buttons

--- js/packages/dys-ui/sandbox/views/HomeView.vue
+++ js/packages/dys-ui/sandbox/views/HomeView.vue
@@ -56,30 +56,38 @@ const onSubmit = handleSubmit((values) => {
       <!-- BUTTON -->
       <div class="grid gap-y-4">
         <h2 class="mb-4 text-2xl font-semibold">Button</h2>
+
+        <h3 class="-mb-1 mt-2 font-semibold">default</h3>
         <div class="flex gap-4">
           <Button>Click Me</Button>
-          <Button variant="destructive">Click Me</Button>
-          <Button variant="outline">Click Me</Button>
-          <Button variant="secondary">Click Me</Button>
-          <Button variant="ghost">Click Me</Button>
-          <Button variant="link">Click Me</Button>
+          <Button variant="destructive">Destructive</Button>
+          <Button variant="outline">Outline</Button>
+          <Button variant="secondary">Secondary</Button>
+          <Button variant="ghost">Ghost</Button>
+          <Button variant="link">Link</Button>
         </div>
+
+        <h3 class="-mb-1 mt-2 font-semibold">sm</h3>
         <div class="flex gap-4">
           <Button size="sm">Click Me</Button>
-          <Button size="sm" variant="destructive">Click Me</Button>
-          <Button size="sm" variant="outline">Click Me</Button>
-          <Button size="sm" variant="secondary">Click Me</Button>
-          <Button size="sm" variant="ghost">Click Me</Button>
-          <Button size="sm" variant="link">Click Me</Button>
+          <Button size="sm" variant="destructive">Destructive</Button>
+          <Button size="sm" variant="outline">Outline</Button>
+          <Button size="sm" variant="secondary">Secondary</Button>
+          <Button size="sm" variant="ghost">Ghost</Button>
+          <Button size="sm" variant="link">Link</Button>
         </div>
+
+        <h3 class="-mb-1 mt-2 font-semibold">lg</h3>
         <div class="flex gap-4">
           <Button size="lg">Click Me</Button>
-          <Button size="lg" variant="destructive">Click Me</Button>
-          <Button size="lg" variant="outline">Click Me</Button>
-          <Button size="lg" variant="secondary">Click Me</Button>
-          <Button size="lg" variant="ghost">Click Me</Button>
-          <Button size="lg" variant="link">Click Me</Button>
+          <Button size="lg" variant="destructive">Destructive</Button>
+          <Button size="lg" variant="outline">Outline</Button>
+          <Button size="lg" variant="secondary">Secondary</Button>
+          <Button size="lg" variant="ghost">Ghost</Button>
+          <Button size="lg" variant="link">Link</Button>
         </div>
+
+        <h3 class="-mb-1 mt-2 font-semibold">icon</h3>
         <div class="flex gap-4">
           <Button size="icon"><span class="i-lucide-alarm-clock size-5" /></Button>
           <Button size="icon" variant="destructive"
