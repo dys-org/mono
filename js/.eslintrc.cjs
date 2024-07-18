9663bfb4b449d848f05c3d2741ca32ffe2988ff1
David Soards <david@soards.me>
2024-07-18 01:26:31 -0400

    better tsconfigs

--- js/.eslintrc.cjs
+++ js/.eslintrc.cjs
@@ -1,25 +1,27 @@
 /* eslint-env node */
-require("@rushstack/eslint-patch/modern-module-resolution");
+require('@rushstack/eslint-patch/modern-module-resolution');
 
 module.exports = {
   root: true,
   extends: [
-    "plugin:vue/vue3-recommended",
-    "eslint:recommended",
-    "@vue/eslint-config-typescript",
-    "@vue/eslint-config-prettier/skip-formatting",
+    'plugin:vue/vue3-recommended',
+    'eslint:recommended',
+    '@vue/eslint-config-typescript',
+    '@vue/eslint-config-prettier/skip-formatting',
   ],
   rules: {
-    "vue/no-undef-components": ["error", { ignorePatterns: [] }],
-    "vue/v-on-event-hyphenation": "off",
+    'vue/no-undef-components': ['error', { ignorePatterns: [] }],
+    'vue/v-on-event-hyphenation': 'off',
+    'vue/multi-word-component-names': 'off',
+    // 'vue/require-default-prop': 'off',
   },
   overrides: [
     {
-      files: ["**/e2e/**/*.{test,spec}.{js,ts,jsx,tsx}"],
-      extends: ["plugin:playwright/recommended"],
+      files: ['**/e2e/**/*.{test,spec}.{js,ts,jsx,tsx}'],
+      extends: ['plugin:playwright/recommended'],
     },
   ],
   parserOptions: {
-    ecmaVersion: "latest",
+    ecmaVersion: 'latest',
   },
 };
